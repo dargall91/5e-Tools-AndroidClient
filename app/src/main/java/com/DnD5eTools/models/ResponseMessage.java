d5630d431f265e2d5d9c81fa43e18b3009ef1bf6
dargall <dargall91@comcast.net>
2025-04-13 17:33:00 -0400

    Feat: Add response wrapper

---
+++ app/src/main/java/com/DnD5eTools/models/ResponseMessage.java
@@ -0,0 +1,23 @@
+package com.DnD5eTools.models;
+
+public class ResponseMessage {
+    private String message;
+
+    private String messageType;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
+}
